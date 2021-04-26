0ccccde7652e3ad087bc6852e7718ac6c3c7b9ef
Samme Janderson <samme.san@gmail.com>
2021-04-26 13:30:44 -0300

    inital setup

---
+++ src/main/java/com/sammjanderson/PerkProgramApi/PerkProgramApiApplication.java
@@ -0,0 +1,13 @@
+package com.sammjanderson.PerkProgramApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerkProgramApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerkProgramApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sammjanderson/PerkProgramApi/PerkProgramApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sammjanderson.PerkProgramApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerkProgramApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
