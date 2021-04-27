06b9be9fba63ce6a6d9be2504689e4a894b64990
Samme Janderson <samme.san@gmail.com>
2021-04-27 17:11:11 -0300

    created client service and controller

--- src/main/java/com/sammjanderson/PerkProgramApi/controller/ClientController.java
+++ src/main/java/com/sammjanderson/PerkProgramApi/controller/ClientController.java
@@ -1,11 +1,48 @@
 package com.sammjanderson.PerkProgramApi.controller;
 
+import com.sammjanderson.PerkProgramApi.dto.request.client.ClientDTO;
+import com.sammjanderson.PerkProgramApi.dto.response.MessageDTO;
+import com.sammjanderson.PerkProgramApi.exception.ClientNotFoundException;
+import com.sammjanderson.PerkProgramApi.service.ClientService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
+@RequestMapping("api/client")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class ClientController {
 
-//TODO create ClientController
+    private ClientService clientService;
+
+    @GetMapping("/all")
+    public List<ClientDTO> listAllClients() {
+        return clientService.listAllClients();
+    }
+
+    //TODO: create dto to client without sensitive information
+    @GetMapping("/{id}")
+    public ClientDTO getClientById(@PathVariable UUID id) throws ClientNotFoundException {
+        return clientService.findClientById(id);
+    }
+
+    //TODO: fix this client should be registered with only the email info
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageDTO registerNewClient(@RequestBody ClientDTO clientDTO) {
+        return clientService.createNewClient(clientDTO);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteClientByID(@PathVariable UUID id) throws ClientNotFoundException {
+        clientService.deleteClientById(id);
+
+    }
 
 }
